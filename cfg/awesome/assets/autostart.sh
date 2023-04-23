118b116c77a26ed884403814cf5e9b9b0e2e39cb
Lyte <89919298+L17E@users.noreply.github.com>
2023-04-23 15:11:09 +0300

    Update autostart.sh

--- cfg/awesome/assets/autostart.sh
+++ cfg/awesome/assets/autostart.sh
@@ -10,6 +10,6 @@ function exe () {
 exe picom --config=./picom/picom.conf -b
 exe $HOME/.config/awesome/scripts/redshift.sh restore
 exe setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle"
-exe syndaemon -i 0.5 -t -K -R -d
+exe syndaemon -i 0.5 -K -R -d
 
 xrdb merge $HOME/.Xresources
